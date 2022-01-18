c4b0e6d897f06f5fabeb84d9f59b600ab7828d89
efsanchezm26 <efsanchezm26>
2022-01-17 20:52:30 -0500

    Creació de proyecto angular

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+// alt 96 `
+
+
+@Component({
+selector : 'app-property-card',
+templateUrl: 'property-card.component.html',
+//template: `<h1>I am a card</h1>`,
+//styles: ['h1{font-weight: normal;}']
+styleUrls: ['property-card.component.css']
+}
+)
+
+export class PropertyCardComponent{
+
+}
